f4c870d03d8467cb37d14b674b61e6d3c8ec9ef0
Dabeer Ahmed <dabeerahmed962@gmail.com>
2023-07-14 16:55:53 +0500

    second commit

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -61,37 +61,3 @@ function PostContent(props) {
   );
 }
 export default PostContent;
-
-// function PostContent(props) {
-//   const { post } = props;
-//   const imagePath = `/images/posts/${post.slug}/${post.image}`;
-//   const customComponents = {
-//     // img: ({ node, ...props }) => (
-//     //   <Image
-//     //       src={`/images/posts/${post.slug}/${props.src}`}
-//     //       alt={props.alt}
-//     //       width={600}
-//     //       height={300}
-//     //     />
-//     //   ),
-//     // };
-//     paragraph(paragraph) {
-//       const { node } = paragraph;
-//       if (node.children[0].type === "image") {
-//         const image = node.children[0];
-
-//         return (
-//           <div className={classes.image}>
-//             <Image
-//               src={`/images/posts/${post.slug}/${image.url}`}
-//               alt={image.alt}
-//               width={600}
-//               height={300}
-//             />
-//           </div>
-//         );
-//       }
-
-//       return <p>{paragraph.children}</p>;
-//     },
-//   };
